ff1e22889c642280fab93cf1ce6aa5660e418d6a
karolina-l <57325023+karolina-l@users.noreply.github.com>
2020-09-05 15:38:30 +0200

    New class (magic) added

--- classes/game.py
+++ classes/game.py
@@ -1,4 +1,5 @@
 import random
+from .magic import Spell
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,10 +26,10 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
-    def generate_spell_dmg(self, i):
-        mgl = self.magic[i]["dmg"] - 5
-        mgh = self.magic[i]["dmg"] + 5
-        return random.randrange(mgl, mgh)
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
 
     def take_damage(self, dmg):
         self.hp -= dmg
@@ -51,12 +52,6 @@ class Person:
     def reduce_mp(self, cost):
         self.mp -= cost
 
-    def get_spell_name(self, i):
-        return self.magic[i]["name"]
-
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
-
     def choose_action(self):
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + "Actions:" + bcolors.ENDC)
@@ -68,5 +63,5 @@ class Person:
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + "Magic:" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell["name"], "(cost:", str(spell["cost"]) + ")")
+            print(str(i) + ":", spell.name, "(cost:", str(spell.cost) + ")")
             i += 1
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from classes.game import Person, bcolors
+from classes.magic import Spell
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 12, "dmg": 80},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+#Create black magic
+fire = Spell("Fire", 10, 100, "black")
+thunder = Spell("Thunder", 10, 100, "black")
+blizzard = Spell("Blizzard", 10, 100, "black")
+metheor = Spell("Metheor", 20, 200, "black")
+quake = Spell("Quake", 14, 140, "black")
 
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 35, magic)
+#Create white magic
+cure = Spell("Cure", 12, 120, "white")
+cura = Spell("Cura", 18, 200, "white")
+
+#Instanciate people
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, metheor, cure, cura])
+enemy = Person(915, 65, 45, 35, [])
 
 running = True
 
@@ -22,9 +31,38 @@ while running:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
         print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+    elif index == 1:
+        player.choose_magic()
+        print("Current MP:", bcolors.OKBLUE + str(player.get_mp()) + bcolors.ENDC)
+        magic_choice = int(input("Choose a spell:")) - 1
+
+        spell = player.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
+
+        current_mp = player.get_mp()
+
+        if spell.cost > current_mp:
+            print(bcolors.FAIL + "\nNot enough MP\n" + bcolors.ENDC)
+            continue
+
+        player.reduce_mp(spell.cost)
+        enemy.take_damage(magic_dmg)
+        print(bcolors.OKBLUE + "\n" + spell.name + " deals", str(magic_dmg), "points of damage." + bcolors.ENDC)
 
     enemy_choice = 1
 
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
     print("Enemy attacks for", enemy_dmg, "points of damage. Player HP", player.get_hp())
+
+    print("----------------------------------")
+    print("Enemy HP:", bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bcolors.ENDC)
+    print("Your HP:", bcolors.OKGREEN + str(player.get_hp()) + "/" + str(player.get_max_hp()) + bcolors.ENDC)
+    print("Your MP", bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + bcolors.ENDC)
+
+    if enemy.get_hp() == 0:
+        print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
+        running = False
+    elif player.get_hp() ==0:
+        print(bcolors.FAIL + "Your enemy has defeated you!" + bcolors.ENDC)
+        running = False
